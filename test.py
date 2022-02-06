0eb03a2efecdf2cacbdbad9d5e71a9272b8a5edd
Emin <emin.134.134@gmail.com>
2022-02-06 19:08:26 +0400

    git ignore changed

--- test.py
+++ test.py
@@ -2,17 +2,11 @@ from AI_project1 import A_star
 
 
 def test_normal():
-    assert A_star((3,2,3),2) == 2
-    assert A_star((2,5,6,72),143) == 7
+    assert A_star((3, 2, 3), 2) == 2
+    assert A_star((2, 5, 6, 72), 143) == 7
+    assert A_star((3, 10, 15), 18) == 4
 
 
 def test_impossible():
-    assert A_star((3,6),2) == -1
-    assert A_star((2,),143) == -1
-
-
-
-    
-
-
-
+    assert A_star((3, 6), 2) == -1
+    assert A_star((2,), 143) == -1
